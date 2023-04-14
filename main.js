9a1d42d3bc08ce4d81fe11ef349dd977677e3cd6
Eric Butler <ebutler@striven.com>
2023-04-14 15:10:47 -0400

    Added electron-builder

--- main.js
+++ main.js
@@ -35,5 +35,5 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') { app.quit() };
+  if (process.platform !== 'darwin') { app.quit() }
 });
---
+++ public/preload.js
@@ -0,0 +1,12 @@
+// const { Titlebar, Color } = require('custom-electron-titlebar');
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     // Title bar implemenation
+//     const titlebar = new Titlebar({
+//         backgroundColor: Color.fromHex("#333333")
+//     });
+
+//     titlebar.updateTitle("Krypt Pad Lite");
+// });
+
+console.log("preload.js loaded ok")
